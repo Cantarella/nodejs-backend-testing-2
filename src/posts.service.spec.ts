44a986403e12e9578a093b0067854675f4656cc5
Cantarella <zdanevich@progressivemedia.ru>
2024-05-23 11:31:14 +0300

    написана реализация для двух тестов и добавлено еще два теста

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -20,13 +20,43 @@ describe('PostsService', () => {
     });
 
     it('should return all posts if called without options', () => {
-      // реализуйте тест-кейс
+      const postList = postsService.findMany();
+
+      expect(postList.length).toEqual(posts.length)
     });
 
     it('should return correct posts for skip and limit options', () => {
-      // реализуйте тест-кейс
+      const postList = postsService.findMany({skip: 1, limit: 2});
+
+      expect(postList).toEqual(expect.arrayContaining([
+        expect.objectContaining({
+          text: 'Post 2',
+        }),
+        expect.objectContaining({
+          text: 'Post 3',
+        })
+      ]))
     });
 
-    // реализуйте недостающие тест-кейсы
+    it('will return 0 elements for a limit of 0', () => {
+      const postList = postsService.findMany({limit: 0});
+
+      expect(postList.length).toEqual(0)
+    })
+
+    it('will return 0 elements if there is too much skip', () => {
+      const postList = postsService.findMany({skip: 50});
+      expect(postList.length).toEqual(0)
+    })
+
+    it('will not return an error if a negative number is passed as skip', () => {
+      const postList = postsService.findMany({skip: -1});
+      expect(postList).toEqual(expect.arrayContaining([
+        expect.objectContaining({
+          id: expect.any(String),
+          text: expect.any(String),
+        })
+      ]))
+    })
   });
 });
